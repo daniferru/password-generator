e6d323d5ba5df0e12ea87c6619cdda31e2ff121e
Daniella <danferrufino@icloud.com>
2023-01-22 22:09:02 -0500

    added README and cleaned up html

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # password-generator
+
+## Description
+- To allow employees to generate random passwords based on criteria they have selected.
+- To create a strong password that provides great security.
+
+## Installation
+1. Go to the following link: https://github.com/daniferru/password-generator.
+2. Clone the repository.
+3. Open th repository in Visual Studio Code.
+
+## Usage
+md
+![Screenshot of the webpage](assets/images/password-web.JPG)
+https://daniferru.github.io/password-generator/
+
+## Tests
+1. Navigate to https://daniferru.github.io/password-generator/
+2. Confirm when clicking the generate button a prompt displays asking number of characters between 8-128.
+3. After inputing a digit, confirm a prompt displays of wanting upper case, lower case, numerical, and special characters.
+4. Once all prompts have finished, confirm a generated password displays in the textbox.
--- script.js
+++ script.js
@@ -2,29 +2,50 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ",", ".", "/", "?", ";", ":"];
+
 var result = [];
+var user = [];
 
 var promptLength = prompt ("Choose a length of at least 8 characters and no more than 128 characters");
+console.log(promptLength);
 var upperCasePrompt = confirm ("Do you want to include uppercase characters?");
+console.log(upperCasePrompt);
 var lowerCasePrompt = confirm ("Do you want to include lowercase characters?");
+console.log(lowerCasePrompt);
 var numericPrompt = confirm ("Do you want to include numeric characters?");
+console.log(numericPrompt);
 var specialCharacters = confirm ("Do you want special characters?")
+console.log(specialCharacters);
 
 if (upperCasePrompt){
     result = result.concat(upperCase)
 } if (lowerCasePrompt){
-    result = result.contact(lowerCase)
+    result = result.concat(lowerCase)
 } if (numericPrompt){
-    result = result.contact(numeric)
+    result = result.concat(numeric)
 } if (specialCharacters){
-    result = result.contact(symbols)
+    result = result.concat(symbols)
 }
-console.log(result)
+//console.log(result)
+
+
+for (var i = 0; i < promptLength; i++) {
+    user.push (result[Math.floor(Math.random() * result.length)]);
 }
+return user.join("");
+}
+
+//function getRandom() {
+   // var resultIndex = Math.floor(Math.random() * result.length);
+   // var resultElement = result[resultIndex];
+   // return resultElement;
+//}
+
 
 
 // Write password to the #password input
@@ -33,7 +54,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
